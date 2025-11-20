fac2af1aef6705eedd638e958ecdd97cab123209
aarrraaaavvvvv1 <aarav.sharma@accredian.com>
2025-11-20 21:13:14 +0530

    Update document_generator.py

--- utils/document_generator.py
+++ utils/document_generator.py
@@ -222,7 +222,7 @@ class WordDocumentGenerator:
         section.right_margin = Inches(0)
         
         # BLUE BACKGROUND
-        for _ in range(12):
+        for _ in range(9):
             para = doc.add_paragraph()
             self._add_blue_background(para)
         
